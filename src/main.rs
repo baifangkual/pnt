616e51fccb024beae181909a4a59cf7cca3e5a4c
baifangkual <baifangkuai@live.com>
2025-07-12 01:57:35 +0800

    chore: 移除多余的env_logger和log依赖
    -- 待完成：
       * ModMainPwd 子命令...
       * 子命令调整conf...
       * need_main_passwd_on_run 配置移到db内
       * help页面渲染逻辑待修改
       * init -tag file 行为 && pnt --tag-file 打开行为
       * 结构需优化...
       * dashboard 底部状态
         * 延申：tick security一定时间丢弃...
       * dashboard-v2页面...
       * theme support...
       * 主要文件加密...

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,5 @@ mod app;
 
 /// pnt bin run
 fn main() -> anyhow::Result<()> {
-    // env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
     app::pnt_run()
 }
